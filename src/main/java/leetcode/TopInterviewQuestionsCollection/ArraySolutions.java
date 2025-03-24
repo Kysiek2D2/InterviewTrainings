5e653d88f14297d4ba1165cc64d4888f74cdb07e
K2D2 <krz.dabkowski@gmail.com>
2025-03-24 15:40:53 +0100

    Update

--- src/main/java/leetcode/TopInterviewQuestionsCollection/ArraySolutions.java
+++ src/main/java/leetcode/TopInterviewQuestionsCollection/ArraySolutions.java
@@ -1,5 +1,6 @@
 package leetcode.TopInterviewQuestionsCollection;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 
@@ -138,6 +139,48 @@ class ArraySolutions {
             result[z] = longer[z];
         }
         return result;
+    }
+
+    /**
+     * Name: Plus One
+     * https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/559/
+     * You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer.
+     * The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
+     * Increment the large integer by one and return the resulting array of digits.
+     * Example:
+     * Input: digits = [1,2,3]
+     * Output: [1,2,4]
+     * Explanation: The array represents the integer 123.
+     * Incrementing by one gives 123 + 1 = 124.
+     * Thus, the result should be [1,2,4].
+     */
+    public int[] plusOne(int[] digits) {
+        int index = digits.length - 1;
+
+        while(true){
+            digits[index] += 1;
+            if(digits[index] == 10){
+                digits[index] = 0;
+                index--;
+            } else {
+                break;
+            }
+            if(index < 0){
+                break;
+            }
+        }
+
+        if(digits[0] != 0){
+            return digits;
+        } else {
+            //case when additional digit is required
+            int[] result = new int[digits.length + 1];
+            result[0] = 1;
+            for(int j = 1; j < result.length; j++){
+                result[j] = digits[j-1];
+            }
+            return result;
+        }
     }
 }
 
